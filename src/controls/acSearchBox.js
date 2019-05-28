1f550118aa457d4c2e01880737e841d121df2adb
sss1998 <sss16211100@163.com>
2019-05-28 14:42:28 +0800

    update searchbox

--- src/controls/acSearchBox.js
+++ src/controls/acSearchBox.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import { Input } from 'antd'
+import { Input,Radio } from 'antd'
 
 const Search=Input.Search
-//type代表搜索类型：学者、机构、领域、论文（成果）
+//type代表搜索类型：专家、机构、论文（成果）
 //handleSearch 响应搜索
 //width 宽度
 class AcSearchBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state={type:props.type,handleSearch:props.handleSearch,width:props.width,value:""}
+    this.state={type:0,handleSearch:props.handleSearch,width:props.width,value:""}
   }
 
+  handleTypeChange= e=>{
+    this.setState({type:e.target.value})
+  }
   render(){
+    var typelist=["专家","机构","论文"];//
+    const {type} =this.state.type;
     return  <div>
-               <Search
-                placeholder={"搜"+this.state.type}
-                onSearch={value=>this.state.handleSearch(value)}
-                style={{width:this.state.width}}
-                allowClear
-                enterButton
-              />
+                <div style={{textAlign:"left"}}>
+                 <Radio.Group value={type} onChange={this.handleTypeChange} >
+                 <Radio.Button value={0}>专家</Radio.Button>
+                 <Radio.Button value={1}>机构</Radio.Button>
+                 <Radio.Button value={2}>论文</Radio.Button>
+                 </Radio.Group>
+                </div>
+                <div>
+                  <Search
+                    placeholder={"搜"+typelist[this.state.type]}
+                    onSearch={value=>this.state.handleSearch(value)}
+                    style={{width:this.state.width,float:"left"}}
+                    allowClear
+                    enterButton
+                  />
+                </div>
+               
             </div>
   } 
 }
