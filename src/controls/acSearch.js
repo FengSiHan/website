5a8f26106b2b712c89a61510919058ac08c149f1
fengsihan <16211102@buaa.edu.cn>
2019-05-28 13:36:58 +0800

    6

--- src/controls/acSearch.js
+++ src/controls/acSearch.js
@@ -11,23 +11,29 @@ class AcSearch extends React.Component
   render()
   {
     return (
-    <div>
-      <Col xl={5} lg={6} md={7} sm={0} xs={0}>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px', border:''}}>
-          <Menu.Item key="1">专家</Menu.Item>
-          <Menu.Item key="2">机构</Menu.Item>
-          <Menu.Item key="3">论文</Menu.Item>
-        </Menu> 
-      </Col>
-      <Col xl={8} lg={9} md={7} sm={0} xs={0}>
-          <Search 
-            placeholder="input search text"
-            enterButton="Search"
-            size="large"
-            onSearch={value => message.info(value + "  invalid")}
-            style={{ lineHeight: '64px', border:'', verticalAlign:'middle'}}
-          />
-      </Col>
+      this.props.homePage === false ? 
+        <div>
+          <Col xl={5} lg={6} md={7} sm={0} xs={0}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px', border:''}}>
+              <Menu.Item key="1">专家</Menu.Item>
+              <Menu.Item key="2">机构</Menu.Item>
+              <Menu.Item key="3">论文</Menu.Item>
+            </Menu> 
+          </Col>
+          <Col xl={8} lg={9} md={7} sm={0} xs={0}>
+              <Search 
+                placeholder="input search text"
+                enterButton="Search"
+                size="large"
+                onSearch={value => message.info(value + "  invalid")}
+                style={{ lineHeight: '64px', border:'', verticalAlign:'middle'}}
+              />
+              
+          </Col>
+        </div>
+    :<div>
+      <Col xl={5} lg={6} md={7} sm={0} xs={0}/>
+      <Col xl={8} lg={9} md={7} sm={0} xs={0}/>
     </div>
     );
   }
